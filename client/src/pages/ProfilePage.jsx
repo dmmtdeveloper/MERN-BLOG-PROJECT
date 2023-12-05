c0f801e0a322d8909145316f4b25a16e37ae7c56
David Martínez Tapia <dmmtapia.ux@gmail.com>
2023-12-04 22:45:53 -0300

    image upload

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,7 +1,34 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getStorage,
+  uploadBytesResumable,
+  ref,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+  signOut,
+} from "../redux/user/userSlice";
+// import { Link } from "react-router-dom";
 
 export default function ProfilePage() {
+  const dispatch = useDispatch();
+  const fileRef = useRef(null);
+  const [formData, setFormData] = useState({});
+  const [image, setImage] = useState(undefined);
+  const [imageError, setImageError] = useState(false);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  //   const [userListings, setUserListings] = useState([]);
+  //   const [showListingsError, setShowListingsError] = useState(false);
+
   const { currentUser } = useSelector((state) => state.user);
 
   const [user, setUser] = useState({
@@ -10,8 +37,97 @@ export default function ProfilePage() {
   });
   const { name, password } = user;
 
-  const handleChange = () => {};
-  const handleSubmit = () => {};
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [image]);
+
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+      },
+      (error) => {
+        console.log(error);
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, profilePicture: downloadURL });
+        });
+      }
+    );
+  };
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  //ACTUALIZR DATOS
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error));
+    }
+  };
+
+  //ELIMINAR CUENTA
+  const handleDeleteAccount = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error));
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await fetch("/api/auth/signout");
+      dispatch(signOut());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -19,21 +135,45 @@ export default function ProfilePage() {
           <div className="flex max-w-sm flex-col text-center lg:text-left">
             <h1 className="text-5xl font-bold">Profile</h1>
             <p className="py-6">
-              Únete a nosotros en esta emocionante travesía fotográfica y
-              personaliza tu experiencia cambiando tu nombre en el blog. ¡Nos
-              vemos pronto! 📸✨
+              Personaliza tu experiencia cambiando tu nombre y agregando tu
+              propia foto de perfil al blog. ¡Te esperamos en esta emocionante
+              travesía fotográfica! 📸✨
             </p>
-            <img
-              src={currentUser.profilePicture}
-              alt="profile"
-              className="h-w-16 mt-2 w-16 cursor-pointer self-center rounded-full"
-            />
           </div>
           <div className="card w-full max-w-sm shrink-0 bg-base-100 shadow-2xl">
             <form onSubmit={handleSubmit} className="card-body">
+              <input
+                onChange={(e) => setImage(e.target.files[0])}
+                type="file"
+                ref={fileRef}
+                hidden
+                accept="image/*"
+              />
+              <img
+                onClick={() => fileRef.current.click()}
+                src={formData.profilePicture || currentUser.profilePicture}
+                alt="profile"
+                className=" h-24 w-24 cursor-pointer  self-center rounded-full border-2 object-cover"
+              />
+
+              <p className="self-center text-sm">
+                {imageError ? (
+                  <span className="text-red-700">
+                    Error uploading image(file size less than 2 MB)
+                  </span>
+                ) : imagePercent > 0 && imagePercent < 100 ? (
+                  <span className="text-slate-700">{`Uploading: ${imagePercent} %`}</span>
+                ) : imagePercent === 100 ? (
+                  <span className="text-green-700">
+                    Image upload successfully
+                  </span>
+                ) : (
+                  ""
+                )}
+              </p>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Email</span>
+                  <span className="label-text">username</span>
                 </label>
                 <input
                   onChange={handleChange}
@@ -43,6 +183,7 @@ export default function ProfilePage() {
                   placeholder="username"
                   className="input input-bordered"
                   required
+                  autoComplete="name"
                 />
               </div>
               <div className="form-control">
@@ -56,6 +197,7 @@ export default function ProfilePage() {
                   id="password"
                   placeholder="password"
                   className="input input-bordered"
+                  autoComplete="password"
                   required
                 />
                 <label className="label"></label>
@@ -63,10 +205,23 @@ export default function ProfilePage() {
               <div className="form-control mt-6 gap-2">
                 <button className="btn btn-primary">UPDATE</button>
               </div>
-            <div className="flex justify-between ">
-                <span className="text-sm cursor-pointer">Delete account</span>
-                <span className="text-sm cursor-pointer">Signout</span>
-            </div>
+              <div className="flex justify-between ">
+                <span
+                  onClick={handleDeleteAccount}
+                  className="cursor-pointer text-sm text-red-500"
+                >
+                  Detele account
+                </span>
+                <span
+                  onClick={handleSignOut}
+                  className="cursor-pointer text-sm text-red-500"
+                >
+                  Sign out
+                </span>
+              </div>
+              <p className="mt-5 text-green-700">
+                {updateSuccess && "User is updated successfully!"}
+              </p>
             </form>
           </div>
         </div>
