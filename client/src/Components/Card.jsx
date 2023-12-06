d6dfd1b27d1cfc0f4365f998f0571f386e6e3c4b
David Martínez Tapia <dmmtapia.ux@gmail.com>
2023-12-06 00:00:23 -0300

    solucion user

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -1,24 +1,26 @@
-import {format} from 'date-fns'
+import { format } from "date-fns";
 
 export default function Card({ title, summary, cover, content, createdAt }) {
-  
   return (
-    <div className="card glass w-96">
-      <figure>
-        <img
-          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="car!"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <p className="text-sm text-left">{format(new Date(createdAt), "dd-MM-yyy")}</p>
-        <p className="text-left">{summary}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Learn now!</button>
+    
+      <div className="card glass  ">
+        <figure>
+          <img
+            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="car!"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p className="text-left text-sm">
+            {format(new Date(createdAt), "dd-MM-yyy")}
+          </p>
+          <p className="text-left">{summary}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">show now!</button>
+          </div>
         </div>
       </div>
-    </div>
+  
   );
 }
-  
